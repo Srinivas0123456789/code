ec7690563a4b5c4b210d98320401b3274942bb3c
Srinivas Gatla <srinivasgatlasrinu@gmail.com>
2026-09-10 21:46:54 +0530

    Update Q11 main program

--- Q11/main.c
+++ Q11/main.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
 
- 
+int Sensor_Read(int *temperature, int *pressure)
+{
+    *temperature = 35;
+    *pressure = 1012;
+
+    return 0;
+}
+
+int main()
+{
+    int temperature;
+    int pressure;
+
+    Sensor_Read(&temperature, &pressure);
+    printf("Temperature = %d\n",temperature);
+    printf("Presuure = %d\n",pressure);
+
+
+
+
+
+    
+    return 0;
+}
